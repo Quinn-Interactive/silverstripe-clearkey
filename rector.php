e3219fd81b2f9fc60a1a28addfee51e93397ace1
Fred Condo <fcondo@quinn.com>
2025-01-24 11:51:40 -0800

    Update QA tooling

---
+++ rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeLevelSetList;
+use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeSetList;
+use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withPhpSets(php74: true)
+    ->withSkip([
+        ChangeSwitchToMatchRector::class,
+    ])
+    ->withImportNames(importShortClasses: false)
+    ->withSets([
+        SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_413,
+        SilverstripeSetList::CODE_QUALITY,
+    ])
+;
