ba119c0674d0b2a70a4d989e2f6a46e0ad9bd5a9
Fred Condo <fred@fredcondo.com>
2025-11-06 15:27:51 -0800

    Update rector config

--- rector.php
+++ rector.php
@@ -5,19 +5,15 @@ declare(strict_types=1);
 use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeLevelSetList;
 use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeSetList;
 use Rector\Config\RectorConfig;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
     ])
-    ->withPhpSets(php83: true)
-    ->withSkip([
-        ChangeSwitchToMatchRector::class,
-    ])
+    ->withPhpSets(php84: true)
     ->withImportNames(importShortClasses: false)
     ->withSets([
-        SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_413,
+        SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_60,
         SilverstripeSetList::CODE_QUALITY,
     ])
 ;
